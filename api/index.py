25dea72fe65cfd9b4884d7c626a94811ebaaaf39
Phil <flowwie53@gmail.com>
2026-06-07 01:07:10 +0100

    Emergency light version - stop serverless timeout

--- api/index.py
+++ api/index.py
@@ -1,21 +1,21 @@
 ﻿from flask import Flask, render_template
 import pandas as pd
-from scraper import scrape_argos_top_deals, quick_ebay_search
 
 app = Flask(__name__, template_folder='../templates')
 
 @app.route("/")
 def index():
-    data = scrape_argos_top_deals()
-    results = None
-    
-    if data:
-        df = pd.DataFrame(data)
-        df = df.sort_values(by='Est. Profit', ascending=False)
-        results = df.to_html(classes='table table-striped', index=False, escape=False)
-    else:
-        results = "<p>Couldn't load deals right now. Try refreshing.</p>"
-
+    results = """
+    <h2>Argos Top Deals Mode</h2>
+    <p>Scraping is currently limited due to serverless timeout.</p>
+    <p>We'll either:</p>
+    <ul>
+        <li>Use caching (next version)</li>
+        <li>Move to better hosting</li>
+    </ul>
+    <br>
+    <p><strong>Current status: Working on a stable solution.</strong></p>
+    """
     return render_template("index.html", results=results)
 
 application = app
