87463d4b920af5700c356c63c9d88e5722c04488
Phil <flowwie53@gmail.com>
2026-06-07 01:05:21 +0100

    Next round - Argos Top Deals with profit comparison

--- api/index.py
+++ api/index.py
@@ -1,19 +1,22 @@
-﻿from flask import Flask, render_template, request
+﻿from flask import Flask, render_template
 import pandas as pd
+from scraper import scrape_argos_top_deals, quick_ebay_search
 
 app = Flask(__name__, template_folder='../templates')
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
+    data = scrape_argos_top_deals()
     results = None
-    message = "Click the button below to load current Argos Top Deals"
+    
+    if data:
+        df = pd.DataFrame(data)
+        df = df.sort_values(by='Est. Profit', ascending=False)
+        results = df.to_html(classes='table table-striped', index=False, escape=False)
+    else:
+        results = "<p>Couldn't load deals right now. Try refreshing.</p>"
 
-    if request.method == "POST":
-        # Very light version - just placeholder for now
-        message = "✅ Top Deals loaded (scraping coming in next update)"
-        results = "<p>Argos Top Deals mode activated.<br><br>We'll load real data without timing out in the next version.</p>"
-
-    return render_template("index.html", results=results, message=message)
+    return render_template("index.html", results=results)
 
 application = app
 
