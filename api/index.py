35f175829ce2d5430628a572d47136e688785eef
Phil <flowwie53@gmail.com>
2026-06-07 02:04:41 +0100

    Fix Render deploy - add gunicorn

--- api/index.py
+++ api/index.py
@@ -1,24 +1,20 @@
 ﻿from flask import Flask, render_template
 import pandas as pd
+from scraper import get_top_deals
 
 app = Flask(__name__, template_folder='../templates')
 
 @app.route("/")
 def index():
-    results = """
-    <h2>Argos Top Deals Mode</h2>
-    <p>Scraping is currently limited due to serverless timeout.</p>
-    <p>We'll either:</p>
-    <ul>
-        <li>Use caching (next version)</li>
-        <li>Move to better hosting</li>
-    </ul>
-    <br>
-    <p><strong>Current status: Working on a stable solution.</strong></p>
-    """
+    data = get_top_deals()
+    if data:
+        df = pd.DataFrame(data)
+        df = df.sort_values(by="Est. Profit £", ascending=False)
+        results = df.to_html(classes='table table-striped', index=False, escape=False)
+    else:
+        results = "<p>Failed to load deals. Refresh the page.</p>"
+
     return render_template("index.html", results=results)
 
-application = app
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080)
